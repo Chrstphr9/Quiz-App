463b1b9a26152fe7a10e4b072cea915fa321c5a1
Chrstphr <somto565@gmail.com>
2023-07-27 12:03:54 +0200

    created a submit function

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,11 +1,32 @@
-import { Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
+import SelectField from "../components/SelectField"
 
 
 const settings = () => {
+
+    
+      const handleSubmit= e => {
+        e.preventDefault();
+      }
+    
+
+
   return (
+    <form onSubmit={handleSubmit}>
     <Typography variant="h2" fontWeight='Bold'>
       Quiz App
     </Typography>
+    <SelectField label='Categoty'/>
+    <SelectField label='Difficulty'/>
+    <SelectField label='Type'/>
+    <Box mt={3} width='100%'>
+    <Button fullWidth variant='contained' type="submit">
+      Get Started
+    </Button>
+    
+    </Box>
+
+    </form>
   )
 }
 
